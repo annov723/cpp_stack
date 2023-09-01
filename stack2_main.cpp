0f73a71d222f9b5ba83f89af1d5801611708a73f
Anna Nowak <annov723@gmail.com>
2023-09-01 17:37:42 +0200

    version 2

--- stack2_main.cpp
+++ stack2_main.cpp
@@ -4,9 +4,26 @@ using namespace std;
 
 int main( void ){
 
-    Stack stack;
-    stack.push( 1 );
-    cout << stack.pop() << endl;
+    try{
+        Stack stack;
+        stack.push( 1 );
+        cout << stack.pop() << endl;
+    }
+    catch( stack_bad_alloc &sba ){
+        cout << "Allocation problem occured." << endl;
+    }
+    catch( stack_size_error &sse ){
+        cout << "Stack of that size cannot exist." << endl;
+    }
+    catch( stack_overflow &sov ){
+        cout << "Stack is too samll for another push." << endl;
+    }
+    catch( stack_empty &se ){
+        cout << "Stack is empty." << endl;
+    }
+    catch( ... ){
+        cout << "Something is wrong..." << endl;
+    }
 
     return 0;
 }
